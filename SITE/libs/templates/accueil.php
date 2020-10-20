6da014950a8c6342dc73f2d76e00eb74d97586a1
brieux_mylene <mylene.brieux@gmail.com>
2020-10-20 10:21:26 +0200

    Modification des fichiers php accueil et menu latéral

---
+++ SITE/libs/templates/accueil.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Halant&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../css/accueil.css">
+
+    <title>
+        Accueil
+    </title>
+</head>
+
+<body>
+    <div class="container-fluid">
+
+        <!-- VOYAGE 1 -->
+        <div class="row">
+
+            <?php include ("menu_lat.php")?>
+
+            <div class="photoprofil1">
+            </div>
+            <h1 class="titrevoyage1">Titre</h1>
+            <div class="encadrevoyage1">
+                <p class="textevoyage1">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla (...)"
+                </p>
+                <a href="" class="lienvoyage1">
+                    Découvrir ce voyage
+                </a>
+            </div>
+        </div>
+
+        <!-- VOYAGE 2 -->
+        <div class="row">
+            <div class="photoprofil2">
+            </div>
+            <h1 class="titrevoyage2">Titre</h1>
+            <div class="encadrevoyage2">
+                <p class="textevoyage2">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla (...)"
+                </p>
+                <a href="" class="lienvoyage2">
+                    Découvrir ce voyage
+                </a>
+            </div>
+        </div>
+
+        <!-- VOYAGE 3 -->
+        <div class="row">
+            <div class="photoprofil3">
+            </div>
+            <h1 class="titrevoyage3">Titre</h1>
+            <div class="encadrevoyage3">
+                <p class="textevoyage3">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla (...)"
+                </p>
+                <a href="" class="lienvoyage3">
+                    Découvrir ce voyage
+                </a>
+            </div>
+        </div>
+
+        <!-- VOYAGE 4 -->
+        <div class="row">
+            <div class="photoprofil4">
+            </div>
+            <h1 class="titrevoyage4">Titre</h1>
+            <div class="encadrevoyage4">
+                <p class="textevoyage4">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla (...)"
+                </p>
+                <a href="" class="lienvoyage4">
+                    Découvrir ce voyage
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
