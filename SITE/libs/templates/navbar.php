b874f91fa6c2d5cd28f44a235c62475c5b0387bb
brieux_mylene <mylene.brieux@gmail.com>
2020-10-22 08:53:12 +0200

    Modification de la navbar php et css

--- SITE/libs/templates/navbar.php
+++ SITE/libs/templates/navbar.php
@@ -1,233 +1,149 @@
 <!DOCTYPE html>
-<html lang="gt">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travelog</title>
 
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../css/navbar.css">
-    <link href="https://fonts.googleapis.com/css2?family=Halant&display=swap" rel="stylesheet">
-    
-    <script src="../jquery/jquery-3.5.1.js" defer></script>
-    <script src="../popper/popper.js" defer></script>
-    <script src="../bootstrap/js/bootstrap.js" defer></script>
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="libs/icofont/icofont.css">
+    <link rel="stylesheet" href="libs/css/navbar.css">
 
+    <script src="libs/jquery/jquery-3.5.1.js" defer></script>
+    <script src="libs/popper/popper.js" defer></script>
+    <script src="libs/bootstrap/js/bootstrap.js" defer></script>
+    <script src="libs/js/index.js" defer></script>
+
+    <title>TRAVELOG</title>
 </head>
+
+
+
 
 <body>
 
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light row">
+    <div class="container mb-4">
+        <header class="row">
 
-            <div class="col-3">
-                <div class="row">
+            <nav class="navbar navbar-expand-md navbar-dark bg-perso nav fixed-top bg-turquoise fixed-top">
 
-                    <div class="col-3 logo">
-                        <!-- Logo -->
-                        <img class="" src="img/logo_blanc2.png" alt="logo">
+                <div class="logo-navbar"><img  src="img/logo_site/logo_blanc2.png"
+                        alt="logo Travelog"></div>
+
+                <div class="col-3">
+
+                    <div class="row">
+                        <a class="navbar-brand d-flex align-items-center" href="index.php?page=home">
+                            <div class=" travelog-navbar">Travelog</div>
+                        </a>
+                        <div class="barre-bl-gauche"></div>
                     </div>
 
-                    <div class="col-9">
-                        <!-- Travelog -->
-                        <div class="navbar-brand travelog" href="#">TRAVELOG</div>
+                </div>
+
+
+
+
+                <!-- BARRE DE RECHERCHE-->
+                <div class="input-group md-form form-sm form-1 pl-0 col-4">
+
+                    <div class="input-group-prepend">
+                        <span class="input-group-text lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+                                aria-hidden="true"></i></span>
                     </div>
+                    <input class="form-control my-0 py-1" type="text" placeholder="Recherche ..." aria-label="Search">
+                </div>
+
+                <div class="barre-bl-droite"></div>
+
+                <img class="cloche" src="img/notifs.png" alt="cloche de notification">
+                <img class="logo-ami" src="img/ami.png" alt="logo ami">
+
+
+
+
+                <!-- BOUTON HAMBURGER-->
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse justify-content-end col-3" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle font-white utilisateur" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Utilisateur
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="mesvoyages.html" target="blank">Mes voyages</a>
+
+                                <a class="dropdown-item" href="monprofil.html" target="blank">Mon profil</a>
+
+                                <?php if(!isset($_SESSION['user'])){ ?>
+                                <a class="dropdown-item" href="index.php?page=figurines">Deconnexion</a>
+                                <?php } ?>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                    <!-- <img src="images/photo01.jpg" alt="Bootstrap" class="img-circle"> -->
+
 
                 </div>
+
+
+
+
+
+                
+
+
+            </nav>
+        </header>
+
+
+
+        <footer>
+            <div class="footer fixed-bottom row">
+
+                <div class="col-4">Conditions générales d'utilisation</div>
+                <div class="barre-bl-bas"></div>
+
+                <div class="col-3">Confidentialité</div>
+                <div class="barre-bl-bas"></div>
+
+                <div class="col-2">Contact</div>
+                <div class="barre-bl-bas"></div>
+
+                <div class="col-2">2020 Travelog</div>
+
+
+
+
+
+
+
+
+
             </div>
-
-
-
-
-            <div class="col-5">
-                <div class="row">
-
-                    <div class="petite-barre-gauche"></div> <!-- petite barre blanche vertical de gauche -->
-
-                    <form class="form-inline my-2 my-lg-0">
-                        <!-- Recherche -->
-                        <input class="form-control mr-sm-2 recherche" type="search" placeholder="Rechercher ..."
-                            aria-label="Search">
-                    </form>
-
-                    <div class="petite-barre-droite"></div> <!-- petite barre blanche vertical de droite -->
-
-                </div>
-            </div>
-
-
-
-
-            <div class="col-4">
-                <div>
-                    <img class="logo-bonhomme" src="img/ami.png" alt="logo">
-                </div>
-
-                <div>
-                    <img class="logo-cloche" src="img/notifs.png" alt="logo">
-                </div>
-
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Mes voyages</a>
-                    <a class="dropdown-item" href="#">Mon profil</a>
-                    <a class="dropdown-item" href="#">Deconnexion</a>
-                </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </nav>
+        </footer>
     </div>
 
 
 
 
 
-    <footer class="container-fluid">
-        <div class="navbar navbar-expand-lg navbar-light row">
-            <div class="container-fluid navbar">
-hello
 
-            </div>
-        </div>
 
-    </footer>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <ul class="navbar-nav mr-auto">
-
-            <li class="nav-item dropdown">
-
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-
-        </ul>
-
-       
-
-
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            Dropdown
-        </a>
-    </div>
-    </nav>
-
-    </div> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- <nav class="navbar">
-
-        <div>
-            <img class="logo" src="img/logo_blanc2.png" alt="logo">
-        </div>
-
-        <h1 class="travelog">TRAVELOG</h1>
-
-        <div class="petite-barre"></div>
-
-        <form class="recherche">
-            <input class="recherche" type="search" placeholder="Rechercher..." aria-label="Search">
-        </form>
-
-        <div class="dropdown">
-            <button class="menuDeroulant" type="button" data-toggle="dropdown">
-                Pseudonyme
-            </button>
-
-            <div class="petite-barre-gauche"></div>
-            
-            <div>
-                <img class="logo-bonhomme" src="img/ami.png" alt="logo">
-            </div>
-
-            <div>
-                <img class="logo-cloche" src="img/notifs.png" alt="logo">
-            </div>
-
-            <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Mes voyages</a>
-                <a class="dropdown-item" href="#">Mon profil</a>
-                <a class="dropdown-item" href="#">Deconnexion</a>
-            </div>
-        </div>
-
-    </nav>
- -->
 
 
 
