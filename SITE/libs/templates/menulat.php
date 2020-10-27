0c023334cc9c91bb04b25cf8fa9a4b3d21ce8b56
mylen <mylene.brieux@gmail.com>
2020-10-27 21:16:42 +0100

    Responsive menu latéral avec filtres

---
+++ SITE/libs/templates/menulat.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Halant&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="../css/menulat.css">
+
+    <title>
+        Menu latéral
+    </title>
+</head>
+
+<body>
+
+            <div class="menulat fixed-left">
+
+                <!-- CONTINENT -->
+                <h6 class="continent"><strong>CONTINENT</strong></h6>
+                    <div class="arrow-wrapper">
+                        <div class="fleche-haut"></div>
+                    </div>
+
+                    <hr>
+                    <div class="Afrique">
+                        <label class="container">Afrique
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="AmeriqueNord">
+                        <label class="container">Amérique du Nord
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="AmeriqueSud">
+                        <label class="container">Amérique du Sud
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="Asie">
+                        <label class="container">Asie
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="Europe">
+                        <label class="container">Europe
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="Oceanie">
+                        <label class="container">Océanie
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                <hr>
+
+                <!-- PAYS -->
+                <h6 class="pays"><strong>PAYS</strong></h6>
+                    <div class="arrow-wrapper">
+                        <div class="fleche-bas"></div>
+                    </div>
+
+                <hr>
+
+                <!-- VILLE -->
+                <h6 class="ville"><strong>VILLE</strong></h6>
+                    <div class="arrow-wrapper">
+                        <div class="fleche-bas"></div>
+                    </div>
+                
+                <hr>
+
+                <!-- LE PLUS -->
+                <h6 class="leplus"><strong>LE PLUS...</strong></h6>
+                    <div class="arrow-wrapper">
+                        <div class="fleche-haut"></div>
+                    </div>
+
+                    <hr>
+
+                    <div class="Populaire">
+                        <label class="container">Populaire
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class="Recent">
+                        <label class="container">Récent
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+            </div>
+            
+</body>
+
+</html>
