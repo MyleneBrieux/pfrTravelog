dfaac47a2ed6e97d7f7582be0cd3dee73e66ead9
brieux_mylene <mylene.brieux@gmail.com>
2020-10-28 16:21:44 +0100

    Création css+responsive et modification php du footer

--- SITE/libs/templates/footer.php
+++ SITE/libs/templates/footer.php
@@ -3,48 +3,32 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../icofont/icofont.css">
-    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
 
-    <script src="../jquery/jquery-3.5.1.js" defer></script>
+    <!-- <script src="../jquery/jquery-3.5.1.js" defer></script>
     <script src="../popper/popper.js" defer></script>
     <script src="../bootstrap/js/bootstrap.js" defer></script>
-    <script src="../js/index.js" defer></script>
+    <script src="../js/index.js" defer></script> -->
 
-    <title>TRAVELOG</title>
+    <link href="https://fonts.googleapis.com/css2?family=Halant&display=swap" rel="stylesheet"> 
+
+    <title>Footer</title>
 </head>
-
-
-
 
 <body>
 
-
     <footer>
-        <div class="footer fixed-bottom row">
+        <div class="footer row">
 
-            <div class="col-4">Conditions générales d'utilisation</div>
-            <div class="barre-bl-bas"></div>
+            <div class="CGU col-3">Conditions générales d'utilisation</div>
 
-            <div class="col-3">Confidentialité</div>
-            <div class="barre-bl-bas"></div>
+            <div class="mentionslegales col-3">Mentions légales</div>
 
-            <div class="col-2">Contact</div>
-            <div class="barre-bl-bas"></div>
+            <div class="contact col-3"><a href="contact.php" class="liencontact">Contact</a></div>
 
-            <div class="col-2">2020 Travelog</div>
-
-
-
-
-
-
-
-
-
+            <div class="copyright col-3">© 2020 Travelog</div>
         </div>
     </footer>
 
