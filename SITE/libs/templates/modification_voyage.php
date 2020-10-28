26ecc48b85bbf1f338cb68de602927b7bae3c735
Yoan <yoann.deco@gmail.com>
2020-10-28 20:39:05 +0100

    modification boutons modification voyage

--- SITE/libs/templates/modification_voyage.php
+++ SITE/libs/templates/modification_voyage.php
@@ -5,6 +5,10 @@
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css" type="text/css">
     <link rel="stylesheet" href="../css/modification_voyage.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--JavaScript-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/20f2b0d45a.js" crossorigin="anonymous"></script>
     <title>Modification de voyage</title>
 </head>
@@ -21,7 +25,7 @@
                     <div class="col-lg-12 col-md-6 col-sm-6 pl-1 col-12">
                         <h5>Titre</h5>
                         <h6>Dates voyage</h6>
-                        <button type="button" class="btn btn-primary">+ Ajouter une étape</button></br>
+                        <button type="button" class="button">+ Ajouter une étape</button></br>
                         <label class="checkbox">
                             <input type="checkbox"> Partager ce voyage uniquement avec mes amis
                         </label>
@@ -183,10 +187,5 @@
             
 
     </div>
-
-    <!--JavaScript-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
