934ac434ee3169d65c198ef27f49380e0ecaecf2
Yoan <yoann.deco@gmail.com>
2020-10-28 16:20:34 +0100

    modif articles sur modification voyage

--- SITE/libs/templates/modification_voyage.php
+++ SITE/libs/templates/modification_voyage.php
@@ -169,6 +169,11 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-12 rectangle">
+                    </div>
+                </div>
+
             </div>
         </div>
 
