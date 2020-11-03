ab0bec1cb6f60fb457879e0bda4eeb13bc30f488
Yoan <yoann.deco@gmail.com>
2020-11-03 15:53:48 +0100

    ajout rectangle description + nom, prenom

--- SITE/libs/templates/monprofil.php
+++ SITE/libs/templates/monprofil.php
@@ -15,9 +15,9 @@
 <body>
     <div class="container-fluid">
         <!--Header-->
-        <div class="header">
+        <header class="header">
             <?php include 'navbar.php' ?>
-        </div>
+        </header>
 
             <div class="row">
                 <div class="col-lg-2 col-md-4 col-sm-4 pl-0 col-12 bg-sable">
@@ -42,8 +42,34 @@
                                     </ul>
                                 </p>
                             </div>
+                            <div class="row mt-3">
+                                <img src="../../img/logos_divers/suivre2.png" alt="logo suivre">Suivre
+                            </div>
+                            <div class="row mt-3">
+                                <img src="../../img/logos_divers/ami_turquoise2.png" alt="logo amis">Ajouter en ami
+                            </div>
+                            <div class="row mt-3 mb-3">
+                            <button type="button" class="button">Contactez moi</button>
+                            </div>
                         </div>
                     </nav>
+                </div>
+
+                <div class="col-lg-10 col-md-8 col-sm-8 col-12 mt-2">
+                    <div class="d-inline-flex p-2 bd-highlight">
+                        <h3>John Doe</h3>
+                    </div>
+                    <div class="row d-flex justify-content-end">
+                        <p class="pr-4 bd-highlight">3 contienents visités</p>
+                        <p class="pr-4 bd-highlight">12 pays visités</p>
+                    </div>
+                    <div class="pl-2 rectangle">
+                        <p class="description">Description : </p>
+                    </div>
+                    <div class="row">
+                        <h3>Mon dernier voyage : </h3>
+                        <h3>Le plus populaire : </h3>
+                    </div>
                 </div>
             </div>
 
