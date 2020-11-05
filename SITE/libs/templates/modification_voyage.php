4a1ec5f0ca890b0f36fb9cbc37c41e457f8a0a85
Yoan <yoann.deco@gmail.com>
2020-11-05 21:16:40 +0100

    problème menu gauche réglé

--- SITE/libs/templates/modification_voyage.php
+++ SITE/libs/templates/modification_voyage.php
@@ -9,7 +9,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/20f2b0d45a.js" crossorigin="anonymous"></script>
     <title>Modification de voyage</title>
 </head>
 <body>
@@ -18,8 +17,9 @@
             <div class="header">
                 <?php include 'navbar.php' ?>
             </div>
+            
 
-        <div class="row d-flex justify-content-center">
+        <div class="row">
             <div class="col-lg-2 col-md-4 col-sm-4 pl-0 col-12 bg-sable">
                 <nav class="menu centrage"></br>
                     <div class="col-lg-12 col-md-6 col-sm-6 pl-1 col-12">
