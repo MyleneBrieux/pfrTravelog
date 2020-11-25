e6c24847fdd17b1f02d8fbb8e20e5c877c5d1eed
Andhromede <71022076+Andhromede@users.noreply.github.com>
2020-11-25 19:55:26 +0100

    upload de la page contact (totalement instable), de sa page traitement et du dossier phpmailer

---
+++ SITE/architecture_en_couche/traitementContact.php
@@ -0,0 +1,118 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once '../libs/phpmailer/Exception.php';
+require_once '../libs/phpmailer/PHPMailer.php';
+require_once '../libs/phpmailer/SMTP.php';
+
+
+
+    $errorMsg ="";
+    $emailMSG = "";
+
+    if(isset ($_POST['submit'])){
+        $message = $_POST['message'];
+        $email = $_POST['email'];
+        $pseudo = $_POST['pseudo'];
+        $subject = "message du site provenant de " .$pseudo;
+
+        if(strlen($message) <10){
+            $errorMsg .= "Votre message est trop court.";
+        }
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+            $errorMsg .= "Votre adresse mail n'est pas conforme.";
+        }
+
+        if(count(explode(' ',$pseudo)) <2){
+            $errorMsg .= "Veuillez saisir votre Pseudonyme.";
+        }
+    
+
+        $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->Mailer="smtp";
+            $mail->SMTPDebug=0;  
+            $mail->SMTPAuth=TRUE;
+            $mail->SMTPSecure="tls";
+            $mail->Port=587;
+            $mail->Host="smtp.gmail.com";
+            $mail->Username="andhromede@gmail.com";
+            $mail->Password="Fm8APqpp";
+            $mail->AddAddress("andhromede@gmail.com", "recipient-name");
+            $mail->SetFrom($email, "from-name");
+            $mail->AddReplyTo($email, "reply-to-name");
+            $mail->Subject=$subject;
+            $content=$message;
+            $mail->MsgHTML($content); 
+
+
+
+        if(!$mail->send()) {
+            echo ("Une erreur s'est produite, votre message n'a pas pu être envoyé");
+            var_dump($mail);
+            } 
+
+        else {
+            echo 'Le message a bien été envoyé !';
+            }
+
+
+
+
+
+
+
+
+            
+
+        // if($errorMsg == ''){
+        //     $subject ="Message envoyé par".$pseudo;
+        //     $emailAdmin = "n.gibilaro@andhromede.planethoster.world";
+        //         $subject = "message du site provenant de " .$pseudo;
+        //     $headers = array ('from' => $email, 'Reply-To' => $email);
+        //     // mail($emailAdmin, $subject, $message, $headers);
+        //     mail( $emailAdmin , $subject , $message , $headers);
+        //     $emailMSG= "Votre message à bien été envoyé !";
+        // }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SITE/libs/templates/contact.php
+++ SITE/libs/templates/contact.php
@@ -15,12 +15,16 @@
 </head>
 
 <body>
+
+
+
     <div class="container-fluid">
 
         <!-- AJOUT DE LA NAVBAR -->
         <div class="row">
             <?php include ("navbar.php")?>
         </div>
+
 
         <div class="row">
             
@@ -31,30 +35,30 @@
                 <p class="texteContact"><strong>Un problème ? Une suggestion ? Contactez-nous !</strong></p>
 
                 <!-- FORMULAIRE -->
-                <form class="formContact">
+                <form action="../../architecture_en_couche/traitementContact.php?action=contact" method="POST" class="formContact">
 
                     <!-- LIGNE 1 AVEC PSEUDO ET EMAIL -->
                     <div class="formLigne1 form-row">
 
                         <!-- PSEUDO -->
                         <div class="form-group col-md-5">
-                            <input type="text" class="Pseudo form-control" id="inputPseudo" placeholder="Pseudo">
+                            <input type="text" class="Pseudo form-control" id="inputPseudo" placeholder="Pseudo" name="pseudo">
                         </div>
 
                         <!-- EMAIL -->
                         <div class="form-group col-md-5">
-                            <input type="email" class="Email form-control" id="inputEmail" placeholder="Adresse email">
+                            <input type="email" class="Email form-control" id="inputEmail" placeholder="Adresse email" name="email">
                         </div>
                     </div>
 
                     <!-- LIGNE 2 AVEC MESSAGE -->
                     <div class="formLigne2 form-group col-md-9">
-                        <input type="text" class="Message form-control" id="inputMessage" placeholder="Saisissez votre message ici...">
+                        <input type="text" class="Message form-control" id="inputMessage" placeholder="Saisissez votre message ici..." name="message">
                     </div>
                 </form>
                 
                 <!-- BOUTON ENVOYER -->
-                <button type="submit" class="boutonEnvoyer btn btn-sm">Envoyer</button>
+                <button type="submit" class="boutonEnvoyer btn btn-sm" name="envoyer" >Envoyer</button>
 
             </div>
        
