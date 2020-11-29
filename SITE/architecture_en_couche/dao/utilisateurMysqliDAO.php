f400ce60b3e8339b0f651620400827120fe184e5
mylen <mylene.brieux@gmail.com>
2020-11-29 19:38:46 +0100

    Modifications utilisateurMysqliDAO.php

--- SITE/architecture_en_couche/dao/utilisateurMysqliDAO.php
+++ SITE/architecture_en_couche/dao/utilisateurMysqliDAO.php
@@ -1,32 +1,55 @@
 <?php
 
 include_once("../metier/Utilisateur.php");
-// include_once("DaoUtilisateurINTERFACE.php");
-
-$user = 'mylene';
-$password = 'afpamy13';
 
     class UtilisateurMysqliDao {
 
+        // public function connexion() {
+        //     $user = 'mylene';
+        //     $password = 'afpamy13';
+        //     $mysqli = new PDO('mysql:host=localhost; dbname=pfrtravelog', $user, $password);
+        //     return $mysqli;
+        // }
+
         public function connexion() {
-            $mysqli = new PDO('mysql:host=localhost; dbname=pfrTravelog', $user, $password);
+            $mysqli= new mysqli('localhost','mylene','afpamy13','pfrtravelog');
             return $mysqli;
         }
 
+        // public function ajoutUtilisateur(string $mail, string $password) {
+        //     $mysqli=$this->connexion();
+        //     $stmt = $mysqli->prepare("insert into utilisateurs(mail,password) values(?,?)");
+        //     $stmt->bindParam(":mail",$mail, ":password", $password);
+        //     $mysqli->exec($stmt);
+        // }
+
         public function ajoutUtilisateur(string $mail, string $password) {
             $mysqli=$this->connexion();
-            $stmt = $mysqli->prepare ("insert into utilisateurs(mail,password) values(?,?)");
-            $stmt->bindParam(":mail",$mail, ":password", $password);
-            $mysqli->exec($stmt);
+            $stmt = $mysqli->prepare("insert into utilisateurs(id,mail,password) values(null,?,?)");
+            $stmt->bind_param("ss",$mail,$password);
+            $stmt->execute();
+            $mysqli->close();
         }
         
+        // public function chercherUtilisateur(string $mail) : ?array {
+        //     $mysqli=$this->connexion();
+        //     $stmt = $mysqli->prepare("select * from utilisateurs where mail=?");
+        //     $stmt->bindParam(":mail",$mail);
+        //     $rs=exec($stmt);
+        //     $data = $rs->fetch(PDO::FETCH_ASSOC);
+        //     $rs->close();
+        //     return $data;
+        // }
+
         public function chercherUtilisateur(string $mail) : ?array {
             $mysqli=$this->connexion();
-            $stmt = $mysqli->prepare ("select * from utilisateurs where mail=?");
-            $stmt->bindParam(":mail",$mail);
-            $rs=exec($stmt);
-            $data = $rs->fetch(PDO::FETCH_ASSOC);
-            $rs->close();
+            $stmt = $mysqli->prepare("select * from utilisateurs where mail=?");
+            $stmt->bind_param("s",$mail);
+            $stmt->execute();
+            $rs = $stmt->get_result();
+            $data = $rs->fetch_array(MYSQLI_ASSOC);
+            $rs->free();
+            $mysqli->close();
             return $data;
         }
 
