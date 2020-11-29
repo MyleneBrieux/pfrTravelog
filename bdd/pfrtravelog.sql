a9d46adb4a366649439758bfe2e106102a596a84
mylen <mylene.brieux@gmail.com>
2020-11-29 20:57:52 +0100

    MISE A JOUR DE LA BASE DE DONNEES

---
+++ bdd/pfrtravelog.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : Dim 29 nov. 2020 à 20:56
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `pfrtravelog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE `commentaires` (
+  `code_comm` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `langues`
+--
+
+CREATE TABLE `langues` (
+  `code_langue` int(11) NOT NULL,
+  `type_langue` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `code_like` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `code_notif` int(11) NOT NULL,
+  `type_notif` varchar(15) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `signalements`
+--
+
+CREATE TABLE `signalements` (
+  `code_signal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `photoprofil` blob DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `nat` varchar(50) DEFAULT NULL,
+  `contact` varchar(3) NOT NULL,
+  `notifmail` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `mail`, `password`, `name`, `firstname`, `photoprofil`, `birthday`, `nat`, `contact`, `notifmail`) VALUES
+(1, 'mylene.brieux@gmail.com', '$2y$10$eQKORdPvoE2WTDj7fzbCL.ror5RystrhUWXXdwzVYcBroqgYh2/D2', '', '', NULL, NULL, NULL, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voyages`
+--
+
+CREATE TABLE `voyages` (
+  `code_voyage` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `couverture` blob NOT NULL,
+  `media` blob DEFAULT NULL,
+  `statut` varchar(6) NOT NULL,
+  `paragraphe` varchar(2000) DEFAULT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vus`
+--
+
+CREATE TABLE `vus` (
+  `code_vue` int(11) NOT NULL,
+  `nombre_vue` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`code_comm`),
+  ADD KEY `cle_secondaire` (`id`);
+
+--
+-- Index pour la table `langues`
+--
+ALTER TABLE `langues`
+  ADD PRIMARY KEY (`code_langue`);
+
+--
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`code_like`);
+
+--
+-- Index pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`code_notif`);
+
+--
+-- Index pour la table `signalements`
+--
+ALTER TABLE `signalements`
+  ADD PRIMARY KEY (`code_signal`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `voyages`
+--
+ALTER TABLE `voyages`
+  ADD PRIMARY KEY (`code_voyage`),
+  ADD KEY `clé_secondaire` (`id`);
+
+--
+-- Index pour la table `vus`
+--
+ALTER TABLE `vus`
+  ADD PRIMARY KEY (`code_vue`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `code_comm` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `langues`
+--
+ALTER TABLE `langues`
+  MODIFY `code_langue` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `code_like` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `code_notif` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `signalements`
+--
+ALTER TABLE `signalements`
+  MODIFY `code_signal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `voyages`
+--
+ALTER TABLE `voyages`
+  MODIFY `code_voyage` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `vus`
+--
+ALTER TABLE `vus`
+  MODIFY `code_vue` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `cle_secondaire` FOREIGN KEY (`id`) REFERENCES `utilisateurs` (`id`);
+
+--
+-- Contraintes pour la table `voyages`
+--
+ALTER TABLE `voyages`
+  ADD CONSTRAINT `clé_secondaire` FOREIGN KEY (`id`) REFERENCES `utilisateurs` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
