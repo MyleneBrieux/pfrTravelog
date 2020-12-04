097d8394104867ffd9ac33085537d7dafd94166f
Romain Wyon <romain_wyon@orange.fr>
2020-12-04 12:10:48 +0100

    Ajout de la page modification voyage en couche controleur

---
+++ SITE/architecture_en_couche/controleur/modification_voyageCONTROLEUR.php
@@ -0,0 +1,11 @@
+<?php
+
+// LIAISON AVEC AUTRES COUCHES //
+include_once("../presentation/modification_voyagePRESENTATION.php");
+// include("../service/VoyageSERVICE.php");
+include("../metier/Voyage.php");
+
+
+modif_headBodyTop();
+modif_corpsPage();
+modif_basPage();
