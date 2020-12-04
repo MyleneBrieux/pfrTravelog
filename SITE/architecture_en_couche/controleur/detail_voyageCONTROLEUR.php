0b6d549c88acee576d0691b375e94e98fb738ef9
Romain Wyon <romain_wyon@orange.fr>
2020-12-04 10:31:07 +0100

    Ajout du detail voyage en couche controleur

---
+++ SITE/architecture_en_couche/controleur/detail_voyageCONTROLEUR.php
@@ -0,0 +1,11 @@
+<?php
+
+// LIAISON AVEC AUTRES COUCHES //
+include_once("../presentation/detail_voyagePRESENTATION.php");
+// include("../service/VoyageSERVICE.php");
+include("../metier/Voyage.php");
+
+
+detail_headBodyTop();
+detail_corpsPage();
+detail_basPage();
