cc0fab19cac942d37acbbc2d34bcfdbfef3c0263
mylen <mylene.brieux@gmail.com>
2020-12-06 20:44:32 +0100

    Réalisation de la pop-up pour les CGU dans la page d'inscription

--- SITE/architecture_en_couche/presentation/inscriptionPRESENTATION.php
+++ SITE/architecture_en_couche/presentation/inscriptionPRESENTATION.php
@@ -9,6 +9,11 @@ function displayPageInscription(){
     displayHeader();
     displayLeftPolaroid();
     displayInscriptionFrame();
+    displayFormInscription();
+    displayLienCGU();
+    popUpCGU();
+    displayInscriptionButton();
+    lienConnexion();
     displayRightPolaroid();
     displayBottomTagContainer();
     displayBottomTagBody();
@@ -75,13 +80,13 @@ function displayLeftPolaroid(){
         <div class="left-polaroid">
             <div id="left-caroussel" class ="carousel slide carousel-fade" data-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-interval="1000">
+                    <div class="carousel-item active" data-interval="750">
                         <img src="../../img/photos/photos_carousels/grece.jpg" alt="left-caroussel slide 1" class="d-block w-100">
                     </div>
-                    <div class="carousel-item" data-interval="1000">
+                    <div class="carousel-item" data-interval="750">
                         <img src="../../img/photos/photos_carousels/hambourg.jpg" alt="Carrousel slide 2" class="d-block w-100">
                     </div>
-                    <div class="carousel-item" data-interval="1000">
+                    <div class="carousel-item" data-interval="750">
                         <img src="../../img/photos/photos_carousels/lac.jpg" alt="Carrousel slide 3" class="d-block w-100">
                     </div>
                 </div>
@@ -94,34 +99,115 @@ function displayInscriptionFrame(){
         '<div class="encadreinscription">
             <div class="introencadreinscription">
                 Inscription
+            </div>';
+}
+
+function displayFormInscription(){
+    echo
+        '<div class="formulaireinscription">
+            <form action="../controleur/inscriptionCONTROLEUR.php?action=inscription" method="post">
+                <div class="form-group">
+                    <input type="text" class="pseudoinscription" name="pseudo" placeholder="Pseudo">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="adressemailinscription" name="mail" placeholder="Adresse email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="motdepasseinscription" name="password" placeholder="Mot de passe">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="confirmationmotdepasseinscription" name="confirmpassword" placeholder="Confirmation du mot de passe">
+                </div>';
+}
+
+function displayLienCGU(){
+    echo
+        '<div class="CGU">
+        <label class="container">J\'accepte les <a href src="" class="liencgu" name="checkcgu" data-toggle="modal" data-target="#CGUPopUp">conditions générales d\'utilisation</a>
+            <input name="checkcgu"type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+    </div>';
+}
+
+function popUpCGU(){
+    echo
+        '<div class="modal fade" id="CGUPopUp">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="cgu-title"><strong>Conditions Générales d\'Utilisation</strong></h4>
+                    </div>
+            
+                    <div class="modal-body">
+                        <h5><strong>Conditions de service</strong></h5>
+                            <p class="text-cgu">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim odio vitae nibh volutpat blandit. 
+                                Integer massa elit, sodales vitae purus quis, tempus aliquet quam. Vivamus pharetra nunc sed est malesuada vulputate. 
+                                Fusce imperdiet lacus sit amet erat vehicula, vitae suscipit ex pharetra. Quisque in velit sem. Mauris malesuada tellus et mi tempus cursus. 
+                                Morbi ut diam aliquet, bibendum libero porta, finibus augue. Maecenas pharetra vulputate sapien in mattis. Pellentesque gravida pharetra facilisis. 
+                                Nullam libero nulla, gravida dapibus mi ut, finibus malesuada neque. Nunc tincidunt sed odio sed pretium. 
+                                Nullam ac dolor sit amet sapien porttitor semper in eu risus. Fusce ante erat, aliquet et ullamcorper in, venenatis sed nisl. 
+                                Donec venenatis dui mi, nec tempus mi ultrices vehicula. Proin blandit mauris urna, quis tempor lacus lobortis at. 
+                                Aenean mattis ipsum augue, a mattis nibh ultrices nec. Sed condimentum, odio eget fermentum viverra, lorem ante elementum justo, at auctor enim lorem in tellus. 
+                                Mauris euismod rhoncus quam, vitae blandit est porttitor non. Aliquam molestie diam sed finibus fringilla. Quisque hendrerit sit amet nisi id blandit. 
+                                Quisque viverra mattis elit, id porttitor tellus ultricies mollis. In vel aliquam ex, quis mollis magna. Nullam aliquet imperdiet laoreet. 
+                                Sed pharetra, ipsum et mollis ultricies, felis arcu semper ipsum, quis vehicula justo metus sed urna. Morbi urna nulla, lobortis sit amet tempus quis, auctor eget elit. 
+                                Nullam laoreet eu quam et aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id leo sit amet velit tincidunt vehicula. 
+                                Nam sollicitudin dignissim pretium. Suspendisse egestas tristique neque, nec elementum lorem vestibulum ut. Quisque egestas ex et dictum mattis. 
+                                Phasellus sed magna sed dolor varius dignissim et ac justo. Integer ligula elit, maximus ut posuere eu, porttitor vel purus.
+                            </p>
+                        <h5><strong>Politique d\'utilisation des données</strong></h5>
+                            <p class="text-cgu">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim odio vitae nibh volutpat blandit. 
+                                Integer massa elit, sodales vitae purus quis, tempus aliquet quam. Vivamus pharetra nunc sed est malesuada vulputate. 
+                                Fusce imperdiet lacus sit amet erat vehicula, vitae suscipit ex pharetra. Quisque in velit sem. Mauris malesuada tellus et mi tempus cursus. 
+                                Morbi ut diam aliquet, bibendum libero porta, finibus augue. Maecenas pharetra vulputate sapien in mattis. Pellentesque gravida pharetra facilisis. 
+                                Nullam libero nulla, gravida dapibus mi ut, finibus malesuada neque. Nunc tincidunt sed odio sed pretium. 
+                                Nullam ac dolor sit amet sapien porttitor semper in eu risus. Fusce ante erat, aliquet et ullamcorper in, venenatis sed nisl. 
+                                Donec venenatis dui mi, nec tempus mi ultrices vehicula. Proin blandit mauris urna, quis tempor lacus lobortis at. 
+                                Aenean mattis ipsum augue, a mattis nibh ultrices nec. Sed condimentum, odio eget fermentum viverra, lorem ante elementum justo, at auctor enim lorem in tellus. 
+                                Mauris euismod rhoncus quam, vitae blandit est porttitor non. Aliquam molestie diam sed finibus fringilla. Quisque hendrerit sit amet nisi id blandit. 
+                                Quisque viverra mattis elit, id porttitor tellus ultricies mollis. In vel aliquam ex, quis mollis magna. Nullam aliquet imperdiet laoreet. 
+                                Sed pharetra, ipsum et mollis ultricies, felis arcu semper ipsum, quis vehicula justo metus sed urna. Morbi urna nulla, lobortis sit amet tempus quis, auctor eget elit. 
+                                Nullam laoreet eu quam et aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id leo sit amet velit tincidunt vehicula. 
+                                Nam sollicitudin dignissim pretium. Suspendisse egestas tristique neque, nec elementum lorem vestibulum ut. Quisque egestas ex et dictum mattis. 
+                                Phasellus sed magna sed dolor varius dignissim et ac justo. Integer ligula elit, maximus ut posuere eu, porttitor vel purus.
+                            </p>
+                        <h5><strong>Standards de la communauté</strong></h5>
+                            <p class="text-cgu">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim odio vitae nibh volutpat blandit. 
+                                Integer massa elit, sodales vitae purus quis, tempus aliquet quam. Vivamus pharetra nunc sed est malesuada vulputate. 
+                                Fusce imperdiet lacus sit amet erat vehicula, vitae suscipit ex pharetra. Quisque in velit sem. Mauris malesuada tellus et mi tempus cursus. 
+                                Morbi ut diam aliquet, bibendum libero porta, finibus augue. Maecenas pharetra vulputate sapien in mattis. Pellentesque gravida pharetra facilisis. 
+                                Nullam libero nulla, gravida dapibus mi ut, finibus malesuada neque. Nunc tincidunt sed odio sed pretium. 
+                                Nullam ac dolor sit amet sapien porttitor semper in eu risus. Fusce ante erat, aliquet et ullamcorper in, venenatis sed nisl. 
+                                Donec venenatis dui mi, nec tempus mi ultrices vehicula. Proin blandit mauris urna, quis tempor lacus lobortis at. 
+                                Aenean mattis ipsum augue, a mattis nibh ultrices nec. Sed condimentum, odio eget fermentum viverra, lorem ante elementum justo, at auctor enim lorem in tellus. 
+                                Mauris euismod rhoncus quam, vitae blandit est porttitor non. Aliquam molestie diam sed finibus fringilla. Quisque hendrerit sit amet nisi id blandit. 
+                                Quisque viverra mattis elit, id porttitor tellus ultricies mollis. In vel aliquam ex, quis mollis magna. Nullam aliquet imperdiet laoreet. 
+                                Sed pharetra, ipsum et mollis ultricies, felis arcu semper ipsum, quis vehicula justo metus sed urna. Morbi urna nulla, lobortis sit amet tempus quis, auctor eget elit. 
+                                Nullam laoreet eu quam et aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id leo sit amet velit tincidunt vehicula. 
+                                Nam sollicitudin dignissim pretium. Suspendisse egestas tristique neque, nec elementum lorem vestibulum ut. Quisque egestas ex et dictum mattis. 
+                                Phasellus sed magna sed dolor varius dignissim et ac justo. Integer ligula elit, maximus ut posuere eu, porttitor vel purus. 
+                            </p>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" id="btn-fermer-cgu" data-dismiss="modal">Fermer</button>
+                    </div>
+                </div>
             </div>
+        </div>';
+}
 
-            <div class="formulaireinscription">
-                <form action="../controleur/inscriptionCONTROLEUR.php?action=inscription" method="post">
-                    <div class="form-group">
-                        <input type="text" class="pseudoinscription" name="pseudo" placeholder="Pseudo">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="adressemailinscription" name="mail" placeholder="Adresse email">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="motdepasseinscription" name="password" placeholder="Mot de passe">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="confirmationmotdepasseinscription" name="confirmpassword" placeholder="Confirmation du mot de passe">
-                    </div>
+function displayInscriptionButton(){
+    echo
+            '<button type="submit" class="boutoninscription"><strong>JE M\'INSCRIS</strong></button>
+        </form>
+    </div>';
+}   
 
-                    <div class="CGU">
-                        <label class="container">J\'accepte les <a href src="" class="liencgu" name="checkcgu" data-toggle="modal" data-target="#CGUPopUp">conditions générales d\'utilisation</a>
-                            <input name="checkcgu"type="checkbox">
-                            <span class="checkmark"></span>
-                        </label>
-                    </div>
-                    <button type="submit" class="boutoninscription"><strong>JE M\'INSCRIS</strong></button>
-                </form>
-            </div>
-
-            <div class="connexion">
+function lienConnexion(){
+    echo                  
+            '<div class="connexion">
                 <strong>Pour se connecter, c\'est par <a href="connexionCONTROLEUR.php" class="lienconnexion">ici</a></strong>
             </div>
         </div>';
@@ -146,13 +232,6 @@ function displayRightPolaroid(){
         </div>
     </div>';
 }
-
-// function displayRightPolaroid(){
-//     echo    
-//             '<img src="../../img/photos/polaroid.png" class="polaroid2"/>
-
-//         </div>';
-// }
 
 function displayBottomTagContainer(){
     echo
