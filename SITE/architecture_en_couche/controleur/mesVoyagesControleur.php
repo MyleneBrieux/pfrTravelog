ccd7e3d0648dd0bd61bc164ce41dd8f045d1b079
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-12-08 18:14:02 +0100

    Modifs pages mes voyages et profil couche controleur

--- SITE/architecture_en_couche/controleur/mesVoyagesControleur.php
+++ SITE/architecture_en_couche/controleur/mesVoyagesControleur.php
@@ -2,7 +2,6 @@
 
 include_once '../presentation/mesVoyagesPresentation.php';
 include '../metier/Utilisateur.php';
-include '../metier/Voyages.php';
 
 voyagesDebut();
 
--- SITE/architecture_en_couche/controleur/monProfilControleur.php
+++ SITE/architecture_en_couche/controleur/monProfilControleur.php
@@ -3,8 +3,7 @@
 include_once '../presentation/monProfilPresentation.php';
 include_once '../service/UtilisateurSERVICE.php';
 include_once '../metier/Utilisateur.php';
-include_once '../service/VoyageSERVICE.php';
-include_once '../metier/Voyage.php';
+
 
 profilDebut();
 
@@ -12,7 +11,7 @@ menuLat();
 
 presentationUser();
 
-voyagesUser();
+voyages();
 
 profilFin();
 
