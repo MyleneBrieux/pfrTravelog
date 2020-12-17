482603d704e3e3b715d35fee8b630402e2dbdfe4
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-12-17 20:46:31 +0100

    Modif controleur de la page mes amis

--- SITE/architecture_en_couche/controleur/mesAmisControleur.php
+++ SITE/architecture_en_couche/controleur/mesAmisControleur.php
@@ -1,7 +1,12 @@
 <?php
+    //session_start();
+    //if(!isset ($_SESSION["mail"])){
+    //     header("Location: connexionCONTROLEUR.php");
+    // }
 
 include_once '../presentation/mesAmisPresentation.php';
 include '../metier/Utilisateur.php';
+include_once '../service/UtilisateurSERVICE.php';
 
 amisDebut();
 
