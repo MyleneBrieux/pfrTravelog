7f2289d27a884eec3f91c1e70485bd80ecf6e358
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-12-17 21:01:05 +0100

    Modif controleur page mon profil

--- SITE/architecture_en_couche/controleur/monProfilControleur.php
+++ SITE/architecture_en_couche/controleur/monProfilControleur.php
@@ -1,8 +1,11 @@
 <?php
+    
+    //session_start();
 
 include_once '../presentation/monProfilPresentation.php';
 include_once '../service/UtilisateurSERVICE.php';
 include_once '../metier/Utilisateur.php';
+include_once '../metier/Voyage.php';
 
 
 profilDebut();
