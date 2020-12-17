04b338c1046bfbbbb67c6199c94bf40999849f9a
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-12-17 21:00:30 +0100

    Modif controleur page mes voyages

--- SITE/architecture_en_couche/controleur/mesVoyagesControleur.php
+++ SITE/architecture_en_couche/controleur/mesVoyagesControleur.php
@@ -1,7 +1,11 @@
 <?php
 
+    //session_start();
+
 include_once '../presentation/mesVoyagesPresentation.php';
-include '../metier/Utilisateur.php';
+include_once '../metier/Utilisateur.php';
+include_once '../metier/Voyage.php';
+include_once '../service/UtilisateurSERVICE.php';
 
 voyagesDebut();
 
