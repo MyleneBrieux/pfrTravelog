34ac270df00b7d31efc8c46af05c9ae9099ef859
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-12-21 16:53:01 +0100

    Début codage back-end

--- SITE/architecture_en_couche/controleur/mesAmisControleur.php
+++ SITE/architecture_en_couche/controleur/mesAmisControleur.php
@@ -1,12 +1,19 @@
 <?php
     //session_start();
-    //if(!isset ($_SESSION["mail"])){
+    //if(!isset ($_SESSION["pseudo"])){
     //     header("Location: connexionCONTROLEUR.php");
     // }
 
 include_once '../presentation/mesAmisPresentation.php';
 include '../metier/Utilisateur.php';
 include_once '../service/UtilisateurSERVICE.php';
+
+//$pseudo = htmlentities($_GET["pseudo"]);
+//try{
+    //$utilisateur = utilisateurSERVICE::chercherPseudo($pseudo);
+//}catch(UtilisateurException $e){
+    //
+//}
 
 amisDebut();
 
