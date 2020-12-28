4747fe5a13a385f398746fd3d82b8c19d8cd635a
Romain Wyon <romain_wyon@orange.fr>
2020-12-28 21:51:14 +0100

    Correction de la fonction d'ajout de voyage en couche service

--- SITE/architecture_en_couche/service/VoyageSERVICE.php
+++ SITE/architecture_en_couche/service/VoyageSERVICE.php
@@ -6,36 +6,36 @@ class VoyageService {
 
 // ajout Voyage
 
-    public function addVoyageService($voyage){
+    public function addVoyageService(/*$codeVoyage, */$titre, $resume, $datedebut, $datefin, $couverture/*, $statut, $likes, $vues*/){
         // try {
-            $addVoyage= new VoyageMysqlDAO;
-            $addVoyage->addVoyageDAO($voyage);
+            $addVoyage= new VoyageMysqliDAO;
+        $addVoyage->addVoyageDAO(/*$codeVoyage, */$titre, $resume, $datedebut, $datefin, $couverture/*, $statut, $likes, $vues*/);
             return $addVoyage;
     //     } catch (addVoyageDaoException $aese){
     //         throw new addVoyageServiceException($aese->getMessage(), $aese->getCode());
     //     }
-    // }
+    }
 
 // suppression Voyage
 
     public function suppVoyageService(int $codeVoyage){
         // try {
-            $suppVoyage= new VoyageMysqlDAO;
+            $suppVoyage= new VoyageMysqliDAO;
             $suppVoyage->suppVoyageDAO($codeVoyage);
     //     } catch (suppVoyageDaoException $sese){
     //         throw new suppVoyageServiceException($sese->getMessage(), $sese->getCode());
     //     }
-    // }
+    }
 
 //Modif Voyage
 
     public function modifVoyageService($voyage){
         // try {
-            $modifVoyage= new VoyageMysqlDAO;
+            $modifVoyage= new VoyageMysqliDAO;
             $modifVoyage->modifVoyageDAO($voyage);
     //     } catch (modifVoyageDaoException $sese){
     //         throw new suppVoyageServiceException($sese->getMessage(), $sese->getCode());
     //     }
-    // }
+    }
 
 }
