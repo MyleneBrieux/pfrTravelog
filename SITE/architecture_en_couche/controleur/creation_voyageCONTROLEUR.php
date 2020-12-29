5e15bae71c334877f743fb8af9462518dfe2c3d9
Romain Wyon <romain_wyon@orange.fr>
2020-12-30 00:10:51 +0100

    Ajout d'etape dans creation voyage en couche controleur

--- SITE/architecture_en_couche/controleur/creation_voyageCONTROLEUR.php
+++ SITE/architecture_en_couche/controleur/creation_voyageCONTROLEUR.php
@@ -3,7 +3,6 @@
 // LIAISON AVEC AUTRES COUCHES //
 include_once("../presentation/creation_voyagePRESENTATION.php");
 include("../service/VoyageSERVICE.php");
-// include("../metier/Voyage.php");
 
 //ajout OOP
 
@@ -16,27 +15,33 @@ if(isset($_GET["action"]) && $_GET["action"] == "creation" && !empty($_POST)){
         && isset($_POST["datefin"]) && !Empty($_POST["datefin"])
         && isset($_POST["couverture"]) && !Empty($_POST["couverture"])){
             
-            $voyage = new Voyage( 
-                $codeVoyage=(int)htmlentities($_POST["codeVoyage"]=null),
-                $titre=htmlentities($_POST["titre"]),
-                $resume=htmlentities($_POST["resume"]),
-                $datedebut=($_POST["datedebut"]),
-                $datefin=($_POST["datefin"]),
-                $couverture=htmlentities($_POST["couverture"]),
-                $statut=htmlentities($_POST["statut"]="Y"),
-                $likes=(int)htmlentities($_POST["likes"]=null),
-                $vues=(int)htmlentities($_POST["vues"]=null)
-            ); 
-            $addVoyage= new VoyageService;
-        $addVoyage->addVoyageService(/*$codeVoyage, */$titre, $resume, $datedebut, $datefin, $couverture/*, $statut, $likes, $vues*/);
-       
-    // try {
-            
-    // }catch(addEmployeServiceException $ese){
-    //     AddMessageErreur($ese->getMessage(),$ese->getCode());
-    // }
+        $voyage = new Etape(
+            $codeVoyage=(int)htmlentities($_POST["codeVoyage"]=null),
+            $titre=htmlentities($_POST["titre"]),
+            $resume=htmlentities($_POST["resume"]),
+            $datedebut=($_POST["datedebut"]),
+            $datefin=($_POST["datefin"]),
+            $couverture=htmlentities($_POST["couverture"]),
+            $statut=htmlentities($_POST["statut"]=null),
+            $likes=(int)htmlentities($_POST["likes"]=null),
+            $vues=(int)htmlentities($_POST["vues"]=null),
+            $codeEtape=(int)htmlentities($_POST["codeEtape"]=null),
+            $sousTitre=htmlentities($_POST["sous_titre"]),
+            $description=htmlentities($_POST["description"]), 
+            $media=htmlentities($_POST["media"]=null),
+            $likesEtape=(int)htmlentities($_POST["likesEtape"]=null)
+        ); 
+        
+        $addEtape= new VoyageService;
+        $addEtape->addEtapeService($sousTitre, $description);
 
-}
+        $addVoyage= new VoyageService;
+        $addVoyage->addVoyageService($titre, $resume, $datedebut, $datefin, $couverture);
+        // if ($passwordOk=$utilisateurservice->passwordVerify($password,$data)){
+            header('Location: detail_voyageCONTROLEUR.php');
+        // }
+    
+    }
 }
 
 
