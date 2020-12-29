3a944158f55de157e39e38fde84d212237322b36
Romain Wyon <romain_wyon@orange.fr>
2020-12-30 00:02:52 +0100

    Ajout de la fonction d'ajout d'etape dans voyage couche service

--- SITE/architecture_en_couche/service/VoyageSERVICE.php
+++ SITE/architecture_en_couche/service/VoyageSERVICE.php
@@ -6,36 +6,30 @@ class VoyageService {
 
 // ajout Voyage
 
-    public function addVoyageService(/*$codeVoyage, */$titre, $resume, $datedebut, $datefin, $couverture/*, $statut, $likes, $vues*/){
-        // try {
-            $addVoyage= new VoyageMysqliDAO;
-        $addVoyage->addVoyageDAO(/*$codeVoyage, */$titre, $resume, $datedebut, $datefin, $couverture/*, $statut, $likes, $vues*/);
-            return $addVoyage;
-    //     } catch (addVoyageDaoException $aese){
-    //         throw new addVoyageServiceException($aese->getMessage(), $aese->getCode());
-    //     }
+    public function addVoyageService($titre, $resume, $datedebut, $datefin, $couverture){
+        $addVoyage= new VoyageMysqliDAO;
+        $addVoyage->addVoyageDAO($titre, $resume, $datedebut, $datefin, $couverture);
+        return $addVoyage;
+    }
+
+    public function addEtapeService($sousTitre, $description){
+        $addEtape= new VoyageMysqliDAO;
+        $addEtape->addEtapeDAO($sousTitre, $description);
+        return $addEtape;
     }
 
 // suppression Voyage
 
     public function suppVoyageService(int $codeVoyage){
-        // try {
             $suppVoyage= new VoyageMysqliDAO;
             $suppVoyage->suppVoyageDAO($codeVoyage);
-    //     } catch (suppVoyageDaoException $sese){
-    //         throw new suppVoyageServiceException($sese->getMessage(), $sese->getCode());
-    //     }
     }
 
 //Modif Voyage
 
     public function modifVoyageService($voyage){
-        // try {
             $modifVoyage= new VoyageMysqliDAO;
             $modifVoyage->modifVoyageDAO($voyage);
-    //     } catch (modifVoyageDaoException $sese){
-    //         throw new suppVoyageServiceException($sese->getMessage(), $sese->getCode());
-    //     }
     }
 
 }
