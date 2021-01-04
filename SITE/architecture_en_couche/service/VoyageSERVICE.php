49274b006349f6da178a38be8bf9d25eb4873bff
Romain Wyon <romain_wyon@orange.fr>
2021-01-04 23:49:59 +0100

    Pour affichage dynamique detail voyage en couche service

--- SITE/architecture_en_couche/service/VoyageSERVICE.php
+++ SITE/architecture_en_couche/service/VoyageSERVICE.php
@@ -5,14 +5,14 @@ include_once("../dao/VoyageMysqliDAO.php");
 class VoyageService {
 
     public function __construct(){
-        $this->VoyageMysqliDao = new VoyageMysqliDao;
+        $this->voyageDao = new VoyageMysqliDAO;
     }
 
 // ajout Voyage
 
     public function addVoyageService($titre, $resume, $datedebut, $datefin, $continent, $pays, $ville, $couverture, $id/*, $codeEtape*/){
         $addVoyage= new VoyageMysqliDAO;
-    $addVoyage->addVoyageDAO($titre, $resume, $datedebut, $datefin, $continent, $pays, $ville, $couverture, $id/*, $codeEtape*/);
+        $addVoyage->addVoyageDAO($titre, $resume, $datedebut, $datefin, $continent, $pays, $ville, $couverture, $id/*, $codeEtape*/);
         return $addVoyage;
     }
 
@@ -20,6 +20,20 @@ class VoyageService {
         $addEtape= new VoyageMysqliDAO;
         $addEtape->addEtapeDAO($sousTitre, $description);
         return $addEtape;
+    }
+
+// afficher les infos du Voyage
+
+    public function afficherLesDetailsVoyageService(int $codeVoyage) : ?array {
+        $detailVoyage = $this->voyageDao ->afficherLesDetailsVoyageDAO($codeVoyage);
+        return $detailVoyage;
+    }
+
+// afficher les infos du Voyage
+
+    public function afficherLesDetailsEtapeService(int $codeEtape) : ?array {
+        $detailEtape = $this->voyageDao ->afficherLesDetailsEtapeDAO($codeEtape);
+        return $detailEtape;
     }
 
 // suppression Voyage
@@ -39,29 +53,29 @@ class VoyageService {
 //Afficher voyages
 
     public function afficherVoyages(){
-        $rs=$this->VoyageMysqliDao->afficherVoyages();
+        $rs=$this->voyageDao->afficherVoyages();
         return $rs;
     }
 
 //Afficher voyages selon pseudo
 
     public function chercherVoyageParPseudo($pseudo){
-        $voyagesService = $this->VoyageMysqliDao->chercherVoyageParPseudo($pseudo);
+        $voyagesService = $this->voyageDao->chercherVoyageParPseudo($pseudo);
         return $voyagesService;
     }
 
 //Compter le nombre de voyages dans la bdd
 
     public function compterVoyages(){
-        $data = $this->VoyageMysqliDao->compterVoyages();
+        $data = $this->voyageDao->compterVoyages();
         return $data;
     }
 
 //Compter le nombre de voyages d'un utilisateur
 
-public function nbVoyagesUtilisateur(){
-    $data = $this->VoyageMysqliDao->nbVoyagesUtilisateur();
-    return $data;
-}
+    public function nbVoyagesUtilisateur(){
+        $data = $this->voyageDao->nbVoyagesUtilisateur();
+        return $data;
+    }
 
 }
