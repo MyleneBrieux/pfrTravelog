ecf0f57b5fdc71d5e2d08158ce41fc0168a015c7
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-01-06 00:41:09 +0100

    Ajout de comentaires à coté des fonctions utilisée + rectifications pour affichage des données du profil

--- SITE/architecture_en_couche/controleur/mesAmisControleur.php
+++ SITE/architecture_en_couche/controleur/mesAmisControleur.php
@@ -2,18 +2,19 @@
 
 session_start();
 
-if(!isset ($_SESSION["pseudo"])){
+include_once '../presentation/mesAmisPresentation.php';
+include_once '../metier/Utilisateur.php';
+include_once '../service/UtilisateurSERVICE.php';
+
+$pseudo = htmlentities($_GET["pseudo"]); //Récupère le pseudo fourni
+$utilisateur = new UtilisateurService();
+
+if(!isset ($_SESSION["pseudo"]) && $_SESSION['pseudo']!==$profil['pseudo']){
     header("Location: connexionCONTROLEUR.php");
 }
 
-include_once '../presentation/mesAmisPresentation.php';
-include '../metier/Utilisateur.php';
-include_once '../service/UtilisateurSERVICE.php';
-
-$pseudo = htmlentities($_GET["pseudo"]);
-$utilisateur = new UtilisateurService();
 //try{
-    $profil = $utilisateur->chercherUtilisateurParPseudo($pseudo);
+    $profil = $utilisateur->chercherUtilisateurParPseudo($pseudo); //Recherche les données de l'utilisateur
     if (isset ($profil['birthday']) && !empty ($profil['birthday'])) {
         $dateNaissance = new DateTime($profil['birthday']);
         $dateAjd = new DateTime();
@@ -22,6 +23,8 @@ $utilisateur = new UtilisateurService();
 //}catch(UtilisateurException $e){
     //
 //}
+
+
 
 amisDebut();
 
