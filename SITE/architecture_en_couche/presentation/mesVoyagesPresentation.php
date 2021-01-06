2edd87f31242ca9c06b7f7e4b80dd5af04caa971
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-01-06 11:59:50 +0100

    essais affichage des voyages d'un utilisateur

--- SITE/architecture_en_couche/presentation/mesVoyagesPresentation.php
+++ SITE/architecture_en_couche/presentation/mesVoyagesPresentation.php
@@ -66,85 +66,81 @@
 </div>';
     }
 
-    function voyage1(){
+    function voyage1($data){ //encadré dynamique
         echo'<div class="row">
         <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil1 ghost" />
-        <h1 class="titrevoyage1">Titre</h1>
+        <h1 class="titrevoyage1">'. $data['titre'] .'</h1>
         <img src="../../../images/photos/trevi.jpg" class="photovoyage1" />
         <div class="encadrevoyage1">
             <p class="textevoyage1">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
+                '. $data['resume'] .'
             </p>
-            <a href="detail_voyage.php" class="lienvoyage1">
+            <a href="detail_voyageCONTROLEUR.php?=code_voyage='. $data['code_voyage'] .'" class="lienvoyage1">
                 Découvrir ce voyage
             </a>
         </div>
     </div>';
     }
         
-    function voyage2(){
-    echo'<div class="row">
-        <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil2 ghost" />
-        <h1 class="titrevoyage2">Titre</h1>
-        <img src="../../../images/photos/beijing_temple_du_ciel.jpg" class="photovoyage2" />
-        <div class="encadrevoyage2">
-            <p class="textevoyage2">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
-            </p>
-            <a href="" class="lienvoyage2">
-                Découvrir ce voyage
-            </a>
-        </div>
-    </div>';
-    }
+    // function voyage2(){ //encadrés brut
+    // echo'<div class="row">
+    //     <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil2 ghost" />
+    //     <h1 class="titrevoyage2">Titre</h1>
+    //     <img src="../../../images/photos/beijing_temple_du_ciel.jpg" class="photovoyage2" />
+    //     <div class="encadrevoyage2">
+    //         <p class="textevoyage2">
+    //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+    //             et dolore magna aliqua.
+    //             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    //             consequat.
+    //             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
+    //         </p>
+    //         <a href="" class="lienvoyage2">
+    //             Découvrir ce voyage
+    //         </a>
+    //     </div>
+    // </div>';
+    // }
 
-    function voyage3(){
-    echo'<div class="row">
-        <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil3 ghost" />
-        <h1 class="titrevoyage3">Titre</h1>
-        <img src="../../../images/photos/st_petersburg_russia.jpg" class="photovoyage3" />
-        <div class="encadrevoyage3">
-            <p class="textevoyage3">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
-            </p>
-            <a href="" class="lienvoyage3">
-                Découvrir ce voyage
-            </a>
-        </div>
-    </div>';
-    }
+    // function voyage3(){
+    // echo'<div class="row">
+    //     <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil3 ghost" />
+    //     <h1 class="titrevoyage3">Titre</h1>
+    //     <img src="../../../images/photos/st_petersburg_russia.jpg" class="photovoyage3" />
+    //     <div class="encadrevoyage3">
+    //         <p class="textevoyage3">
+    //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+    //             et dolore magna aliqua.
+    //             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    //             consequat.
+    //             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
+    //         </p>
+    //         <a href="" class="lienvoyage3">
+    //             Découvrir ce voyage
+    //         </a>
+    //     </div>
+    // </div>';
+    // }
 
-    function voyage4(){
-    echo'<div class="row">
-        <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil4 ghost" />
-        <h1 class="titrevoyage4">Titre</h1>
-        <img src="../../../images/photos/palais_gyeongbok.jpg" class="photovoyage4" />
-        <div class="encadrevoyage4">
-            <p class="textevoyage4">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
-            </p>
-            <a href="" class="lienvoyage4">
-                Découvrir ce voyage
-            </a>
-        </div>
-    </div>';
-    }
+    // function voyage4(){
+    // echo'<div class="row">
+    //     <img src="../../../images/photos/photo_profil_detail_voyage.jpg" class="photoprofil4 ghost" />
+    //     <h1 class="titrevoyage4">Titre</h1>
+    //     <img src="../../../images/photos/palais_gyeongbok.jpg" class="photovoyage4" />
+    //     <div class="encadrevoyage4">
+    //         <p class="textevoyage4">
+    //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+    //             et dolore magna aliqua.
+    //             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    //             consequat.
+    //             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
+    //         </p>
+    //         <a href="" class="lienvoyage4">
+    //             Découvrir ce voyage
+    //         </a>
+    //     </div>
+    // </div>';
+    // }
 
     function nbPages(){
         echo'<div class="row">
@@ -169,11 +165,11 @@
         voyagesHeader();
     }
 
-    function afficherVoyages(){
-        voyage1();
-        voyage2();
-        voyage3();
-        voyage4();
+    function afficherVoyages($data){
+        voyage1($data);
+        // voyage2();
+        // voyage3();
+        // voyage4();
     }
 
     function voyagesFin(){
