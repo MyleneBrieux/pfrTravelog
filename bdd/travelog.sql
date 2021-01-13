efd9a8cd2f4d7e958af4375544d690595ea37543
Romain Wyon <romain_wyon@orange.fr>
2021-01-13 22:52:23 +0100

    Maj BBD (ajout de la table Images et suppression de la colonne media)

--- bdd/travelog.sql
+++ bdd/travelog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : Dim 03 jan. 2021 à 21:57
--- Version du serveur :  10.4.14-MariaDB
--- Version de PHP : 7.2.34
+-- Généré le : mer. 13 jan. 2021 à 22:25
+-- Version du serveur :  10.4.16-MariaDB
+-- Version de PHP : 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `travelog`
+-- Base de données : `pfrtravelog`
 --
 
 -- --------------------------------------------------------
@@ -55,9 +55,21 @@ CREATE TABLE `etape` (
   `code_etape` int(11) NOT NULL,
   `sous_titre` varchar(40) NOT NULL,
   `description` varchar(500) NOT NULL,
-  `media` mediumblob NOT NULL,
   `likesEtape` int(11) DEFAULT NULL,
   `code_comm` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images`
+--
+
+CREATE TABLE `images` (
+  `code_img` int(11) NOT NULL,
+  `image` longblob NOT NULL,
+  `code_voyage` int(11) NOT NULL,
+  `code_etape` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -134,7 +146,7 @@ CREATE TABLE `utilisateurs` (
   `mail` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
-  `photoprofil` mediumblob DEFAULT NULL,
+  `photoprofil` longblob DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `nation` varchar(50) DEFAULT NULL,
   `contact` varchar(1) NOT NULL,
@@ -147,7 +159,10 @@ CREATE TABLE `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`id`, `pseudo`, `mail`, `password`, `description`, `photoprofil`, `birthday`, `nation`, `contact`, `notifmail`, `code_langue`) VALUES
-(38, 'mylene', 'mylene.brieux@gmail.com', '$2y$10$ZtuxhdTaRYssZHfUE/amN.Vg3VS4mBCwt1kV.NWgNSbAFT.oXFpom', NULL, 0x70686f746f, NULL, NULL, 'Y', 'Y', 1);
+(38, 'mylene', 'mylene.brieux@gmail.com', '$2y$10$ZtuxhdTaRYssZHfUE/amN.Vg3VS4mBCwt1kV.NWgNSbAFT.oXFpom', NULL, 0x70686f746f, NULL, NULL, 'Y', 'Y', 1),
+(39, 'karma', 'romain_wyon@orange.fr', '$2y$10$Au2zN2kQEKylV7TU21xW7.E5gtjdLMYSzMmENCBtelW1s31YY2RTC', '', '', '1999-01-11', 'Fran&ccedil;ais', 'Y', 'Y', 2),
+(40, 'karma_deux', 'romain@orange.fr', '$2y$10$d89zho/bkViGNv3ZZEFCNOvms8EFUoFQlWePml14Sda1b9w/J1LVG', NULL, 0x70686f746f, NULL, NULL, 'Y', 'Y', 1),
+(41, 'karma_trois', 'karma_trois@azerty.com', '$2y$10$VsExGZnbGxJ/OxVczCKGiuUduFFsW.QHYmC3HaEb5f5GGvqGTYKIS', NULL, 0x70686f746f, NULL, NULL, 'Y', 'Y', 1);
 
 -- --------------------------------------------------------
 
@@ -165,7 +180,7 @@ CREATE TABLE `voyages` (
   `pays` varchar(50) NOT NULL,
   `ville` varchar(50) NOT NULL,
   `couverture` mediumblob NOT NULL,
-  `statut` varchar(6) NOT NULL,
+  `statut` varchar(6) NOT NULL DEFAULT '''N''',
   `likes` int(12) DEFAULT NULL,
   `vues` int(12) DEFAULT NULL,
   `id` int(11) NOT NULL,
@@ -196,6 +211,14 @@ ALTER TABLE `demande_ami`
 ALTER TABLE `etape`
   ADD PRIMARY KEY (`code_etape`),
   ADD KEY `code_comm` (`code_comm`);
+
+--
+-- Index pour la table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`code_img`),
+  ADD KEY `codeEtape` (`code_etape`),
+  ADD KEY `codeVoyage` (`code_voyage`);
 
 --
 -- Index pour la table `langues`
@@ -257,6 +280,12 @@ ALTER TABLE `etape`
   MODIFY `code_etape` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `images`
+--
+ALTER TABLE `images`
+  MODIFY `code_img` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `langues`
 --
 ALTER TABLE `langues`
@@ -278,7 +307,7 @@ ALTER TABLE `signalements`
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT pour la table `voyages`
@@ -301,6 +330,13 @@ ALTER TABLE `commentaires`
 --
 ALTER TABLE `etape`
   ADD CONSTRAINT `code_comm` FOREIGN KEY (`code_comm`) REFERENCES `commentaires` (`code_comm`);
+
+--
+-- Contraintes pour la table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `codeEtape` FOREIGN KEY (`code_etape`) REFERENCES `etape` (`code_etape`),
+  ADD CONSTRAINT `codeVoyage` FOREIGN KEY (`code_voyage`) REFERENCES `voyages` (`code_voyage`);
 
 --
 -- Contraintes pour la table `signalements`
