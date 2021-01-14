94f952e1e6401a9c3b4444546dc5ecd55b541356
Yoan <yoann.deco@gmail.com>
2021-01-14 16:11:12 +0100

    Ajout script test delete ami

---
+++ SITE/libs/script_js/scriptModalPageAmis.js
@@ -0,0 +1,11 @@
+$('button.btnDelete').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).closest('tr').data('id');
+    $('#myModal').data('id', id).modal('show');
+});
+
+$('#btnDelteYes').click(function () {
+    var id = $('#myModal').data('id');
+    $('[data-id=' + id + ']').remove();
+    $('#myModal').modal('hide');
+});
