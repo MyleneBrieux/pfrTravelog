106491d3b321f31d98dca9b1bfbf124d92a4c03c
Romain Wyon <romain_wyon@orange.fr>
2021-01-18 09:06:50 +0100

    Modification de la bdd table commentaires

--- bdd/travelog.sql
+++ bdd/travelog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 15 jan. 2021 à 10:49
--- Version du serveur :  10.4.14-MariaDB
--- Version de PHP : 7.2.34
+-- Généré le : lun. 18 jan. 2021 à 08:52
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,15 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `commentaires` (
   `code_comm` int(11) NOT NULL,
   `commentaire` varchar(500) NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `code_etape` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `commentaires`
 --
 
-INSERT INTO `commentaires` (`code_comm`, `commentaire`, `id`) VALUES
-(1, 'Je suis un commentaire test.', 42);
+INSERT INTO `commentaires` (`code_comm`, `commentaire`, `id`, `code_etape`) VALUES
+(1, 'Je suis un commentaire test.', 42, 0),
+(2, 'cc', 40, 5),
+(3, '2e commentaire', 41, 5);
 
 -- --------------------------------------------------------
 
@@ -73,25 +76,24 @@ INSERT INTO `demande_ami` (`code_ami`, `id_ami`, `id`, `accepte`) VALUES
 CREATE TABLE `etape` (
   `code_etape` int(11) NOT NULL,
   `sous_titre` varchar(40) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `code_comm` int(11) DEFAULT NULL
+  `description` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `etape`
 --
 
-INSERT INTO `etape` (`code_etape`, `sous_titre`, `description`, `code_comm`) VALUES
-(1, '&eacute;tape 1', 'Premi&egrave;re &eacute;tape', NULL),
-(2, '&eacute;tape 1', 'Premi&egrave;re &eacute;tape', NULL),
-(3, 'Lorem ipsum dolor sit amet, consectetur ', 'Nullam et congue ligula. Vestibulum sapien sem, suscipit non dui eget, vulputate pellentesque nisi. Duis sit amet leo scelerisque nulla finibus hendrerit. Cras eget magna quis leo blandit consequat posuere vitae quam. Nulla sodales consequat massa eget rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ', NULL),
-(4, 'Lorem ipsum dolor sit amet, consectetur ', 'Nullam et congue ligula. Vestibulum sapien sem, suscipit non dui eget, vulputate pellentesque nisi. Duis sit amet leo scelerisque nulla finibus hendrerit. Cras eget magna quis leo blandit consequat posuere vitae quam. Nulla sodales consequat massa eget rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ', NULL),
-(5, 'Cras ac ex felis. ', 'Aenean quis elementum enim, sit amet convallis enim. Mauris eget tortor porttitor, tempor orci at, efficitur sem. Cras molestie, nisi ullamcorper hendrerit facilisis, dolor lectus feugiat nunc, eget tempor ipsum nibh eget massa. Vestibulum in gravida ligula. Ut suscipit nisl eget nulla pretium, in bibendum velit posuere. Cras porta diam eget consectetur ultrices. ', NULL),
-(6, 'In hac habitasse platea dictumst. ', 'Nullam est orci, pretium rutrum tempor non, volutpat eu ipsum. Vestibulum id urna eget ante consectetur fringilla vitae sed lorem. Curabitur auctor interdum lobortis. Quisque ullamcorper mi tellus, vehicula molestie ante pellentesque id. Curabitur ac ante non tortor dignissim malesuada eu eu tortor. ', NULL),
-(7, 'Donec ut nulla sed velit imperdiet vehic', 'Etiam ut lectus pellentesque, fringilla erat et, ultricies tellus. Nulla commodo ornare odio, eget sagittis purus feugiat ac. Vivamus et sem vitae purus sagittis consectetur nec ut risus. Morbi consectetur, quam quis sodales viverra, odio neque dignissim arcu, in consequat lorem metus id quam. Pellentesque luctus malesuada fermentum. Duis pulvinar libero at sapien ornare, nec sollicitudin purus luctus. ', NULL),
-(8, ' Donec nisi ante, scelerisque non libero', 'Aenean fringilla lacinia suscipit. Fusce tempor sit amet ipsum vitae iaculis. Donec lacinia posuere vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas euismod leo vitae pharetra egestas. Nam tincidunt orci eu purus commodo feugiat. Etiam sit amet laoreet dui. Pellentesque tincidunt vitae arcu non sodales. ', NULL),
-(9, 'Duis quis auctor ipsum.', 'Vivamus eros quam, gravida id urna sed, accumsan pharetra massa. Sed id lobortis quam. Suspendisse lacinia tempus mattis. In imperdiet neque sed nibh gravida ullamcorper. Vestibulum eu efficitur enim, id consequat dui. Duis vestibulum rhoncus diam at suscipit. Morbi faucibus, diam ac ultricies venenatis, urna odio ornare odio, id hendrerit neque tortor sit amet ligula. Curabitur ut diam et sem molestie tristique vitae ac nisi. ', NULL),
-(10, 'Vivamus nec leo a libero eleifend commod', 'n quis dui sit amet purus tempor pretium. Donec iaculis libero nec lectus varius, vel mattis odio facilisis. Cras in leo quam. Suspendisse malesuada ornare est, quis varius ante dapibus at. Morbi nulla est, finibus vitae condimentum in, sagittis nec felis. Sed et mauris nec urna faucibus interdum. Aenean dictum in mauris sit amet sodales. Maecenas pharetra semper dui, sed facilisis eros scelerisque ac. ', NULL);
+INSERT INTO `etape` (`code_etape`, `sous_titre`, `description`) VALUES
+(1, '&eacute;tape 1', 'Premi&egrave;re &eacute;tape'),
+(2, '&eacute;tape 1', 'Premi&egrave;re &eacute;tape'),
+(3, 'Lorem ipsum dolor sit amet, consectetur ', 'Nullam et congue ligula. Vestibulum sapien sem, suscipit non dui eget, vulputate pellentesque nisi. Duis sit amet leo scelerisque nulla finibus hendrerit. Cras eget magna quis leo blandit consequat posuere vitae quam. Nulla sodales consequat massa eget rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. '),
+(4, 'Lorem ipsum dolor sit amet, consectetur ', 'Nullam et congue ligula. Vestibulum sapien sem, suscipit non dui eget, vulputate pellentesque nisi. Duis sit amet leo scelerisque nulla finibus hendrerit. Cras eget magna quis leo blandit consequat posuere vitae quam. Nulla sodales consequat massa eget rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. '),
+(5, 'Cras ac ex felis. ', 'Aenean quis elementum enim, sit amet convallis enim. Mauris eget tortor porttitor, tempor orci at, efficitur sem. Cras molestie, nisi ullamcorper hendrerit facilisis, dolor lectus feugiat nunc, eget tempor ipsum nibh eget massa. Vestibulum in gravida ligula. Ut suscipit nisl eget nulla pretium, in bibendum velit posuere. Cras porta diam eget consectetur ultrices. '),
+(6, 'In hac habitasse platea dictumst. ', 'Nullam est orci, pretium rutrum tempor non, volutpat eu ipsum. Vestibulum id urna eget ante consectetur fringilla vitae sed lorem. Curabitur auctor interdum lobortis. Quisque ullamcorper mi tellus, vehicula molestie ante pellentesque id. Curabitur ac ante non tortor dignissim malesuada eu eu tortor. '),
+(7, 'Donec ut nulla sed velit imperdiet vehic', 'Etiam ut lectus pellentesque, fringilla erat et, ultricies tellus. Nulla commodo ornare odio, eget sagittis purus feugiat ac. Vivamus et sem vitae purus sagittis consectetur nec ut risus. Morbi consectetur, quam quis sodales viverra, odio neque dignissim arcu, in consequat lorem metus id quam. Pellentesque luctus malesuada fermentum. Duis pulvinar libero at sapien ornare, nec sollicitudin purus luctus. '),
+(8, ' Donec nisi ante, scelerisque non libero', 'Aenean fringilla lacinia suscipit. Fusce tempor sit amet ipsum vitae iaculis. Donec lacinia posuere vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas euismod leo vitae pharetra egestas. Nam tincidunt orci eu purus commodo feugiat. Etiam sit amet laoreet dui. Pellentesque tincidunt vitae arcu non sodales. '),
+(9, 'Duis quis auctor ipsum.', 'Vivamus eros quam, gravida id urna sed, accumsan pharetra massa. Sed id lobortis quam. Suspendisse lacinia tempus mattis. In imperdiet neque sed nibh gravida ullamcorper. Vestibulum eu efficitur enim, id consequat dui. Duis vestibulum rhoncus diam at suscipit. Morbi faucibus, diam ac ultricies venenatis, urna odio ornare odio, id hendrerit neque tortor sit amet ligula. Curabitur ut diam et sem molestie tristique vitae ac nisi. '),
+(10, 'Vivamus nec leo a libero eleifend commod', 'n quis dui sit amet purus tempor pretium. Donec iaculis libero nec lectus varius, vel mattis odio facilisis. Cras in leo quam. Suspendisse malesuada ornare est, quis varius ante dapibus at. Morbi nulla est, finibus vitae condimentum in, sagittis nec felis. Sed et mauris nec urna faucibus interdum. Aenean dictum in mauris sit amet sodales. Maecenas pharetra semper dui, sed facilisis eros scelerisque ac. ');
 
 -- --------------------------------------------------------
 
@@ -142,6 +144,18 @@ INSERT INTO `langues` (`code_langue`, `type_langue`) VALUES
 (18, 'thai'),
 (19, 'bengali'),
 (20, 'pendjabi');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id_like` int(11) NOT NULL,
+  `code_voyage` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -239,12 +253,12 @@ CREATE TABLE `voyages` (
 
 INSERT INTO `voyages` (`code_voyage`, `titre`, `resume`, `date_debut`, `date_fin`, `continent`, `pays`, `ville`, `couverture`, `statut`, `likes`, `vues`, `id`, `code_etape`) VALUES
 (4, 'Lorem ipsum dolor sit amet, consectetur adipiscing', 'Sed lacinia, tortor id scelerisque pharetra, sapien libero vulputate sapien, id mattis augue nisl in mi. Mauris id porta nibh. Pellentesque eleifend ullamcorper tortor. Morbi ligula lacus, egestas a ante eget, facilisis vestibulum massa. Praesent vestibulum tempus egestas.', '2019-06-17', '2019-06-23', 'Afrique', 'Maroc', 'Agadir', 0x6d61726f632e6a7067, 'Public', 0, 0, 38, 4),
-(5, 'Cras mattis diam ut cursus sollicitudin. ', 'Nulla placerat, orci ac pellentesque aliquet, erat nisi rhoncus dolor, non rhoncus nunc ex sit amet felis. Etiam rhoncus, nisi in feugiat aliquet, lacus ipsum dapibus leo, in scelerisque erat lectus eu ex.', '2020-12-18', '2020-12-20', 'Europe', 'Angleterre', 'Londres', 0x6c6f6e647265732e6a7067, 'Public', 0, 0, 38, 5),
+(5, 'Cras mattis diam ut cursus sollicitudin. ', 'Nulla placerat, orci ac pellentesque aliquet, erat nisi rhoncus dolor, non rhoncus nunc ex sit amet felis. Etiam rhoncus, nisi in feugiat aliquet, lacus ipsum dapibus leo, in scelerisque erat lectus eu ex.', '2020-12-18', '2020-12-20', 'Europe', 'Angleterre', 'Londres', 0x6c6f6e647265732e6a7067, 'Public', 0, 32, 38, 5),
 (6, 'Ut vestibulum odio tincidunt, condimentum eros a, ', 'Pellentesque tristique, velit sit amet dignissim aliquet, nisi massa elementum nunc, quis eleifend magna enim feugiat nibh. ', '2020-05-14', '2020-05-17', 'Europe', 'Allemagne', 'Hambourg', 0x68616d626f7572672e6a7067, 'Public', 0, 0, 38, 6),
 (7, 'Morbi metus purus, venenatis nec placerat vel, max', 'Nullam sagittis, nisi non aliquet cursus, lorem libero iaculis nulla, in vestibulum dolor neque sit amet elit. Nam dignissim urna nisl, nec sollicitudin arcu sodales non. Phasellus convallis, felis eu semper cursus, neque massa venenatis nisi, eu porttitor magna sem commodo eros. ', '2020-07-01', '2020-07-31', 'Am&eacute;rique du S', 'P&eacute;rou', 'Lima', 0x7065726f752e6a7067, 'Public', 0, 0, 45, 7),
 (8, 'Sed molestie finibus nisl, ac viverra nulla commod', 'Vivamus ac tincidunt ipsum, vitae semper libero. Cras faucibus eu magna ut accumsan. In felis tellus, mattis eu ex molestie, malesuada tincidunt sem. ', '2019-08-01', '2019-08-31', 'Asie', 'Japon', 'Tokyo', 0x6a61706f6e2e6a7067, 'Public', 0, 0, 45, 8),
 (9, 'Aliquam a pellentesque ante. ', 'Praesent eget sollicitudin diam, eget pulvinar sapien. Mauris dictum maximus sollicitudin. Mauris quis laoreet elit.', '2020-05-04', '2020-05-25', 'Am&eacute;rique du N', 'Etats-Unis', 'Dallas', 0x65746174732d756e69732e6a7067, 'Public', 0, 0, 46, 9),
-(10, 'Phasellus convallis gravida laoreet. ', 'Sed eu mauris vestibulum, cursus mauris sit amet, laoreet ipsum. Morbi rutrum lorem eros, ac vestibulum est tincidunt in. ', '2021-01-02', '2021-01-03', 'Europe', 'Italie', 'Rome', 0x74726576692e6a7067, 'Public', 0, 0, 46, 10);
+(10, 'Phasellus convallis gravida laoreet. ', 'Sed eu mauris vestibulum, cursus mauris sit amet, laoreet ipsum. Morbi rutrum lorem eros, ac vestibulum est tincidunt in. ', '2021-01-02', '2021-01-03', 'Europe', 'Italie', 'Rome', 0x74726576692e6a7067, 'Public', 0, 6, 46, 10);
 
 --
 -- Index pour les tables déchargées
@@ -255,7 +269,8 @@ INSERT INTO `voyages` (`code_voyage`, `titre`, `resume`, `date_debut`, `date_fin
 --
 ALTER TABLE `commentaires`
   ADD PRIMARY KEY (`code_comm`),
-  ADD KEY `cle_secondaire` (`id`);
+  ADD KEY `cle_secondaire` (`id`),
+  ADD KEY `code_etape` (`code_etape`);
 
 --
 -- Index pour la table `demande_ami`
@@ -269,8 +284,7 @@ ALTER TABLE `demande_ami`
 -- Index pour la table `etape`
 --
 ALTER TABLE `etape`
-  ADD PRIMARY KEY (`code_etape`),
-  ADD KEY `code_comm` (`code_comm`);
+  ADD PRIMARY KEY (`code_etape`);
 
 --
 -- Index pour la table `images`
@@ -285,6 +299,14 @@ ALTER TABLE `images`
 --
 ALTER TABLE `langues`
   ADD PRIMARY KEY (`code_langue`);
+
+--
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id_like`),
+  ADD KEY `IdVoyage` (`code_voyage`),
+  ADD KEY `IdUser` (`id`);
 
 --
 -- Index pour la table `notifications`
@@ -325,7 +347,7 @@ ALTER TABLE `voyages`
 -- AUTO_INCREMENT pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  MODIFY `code_comm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `code_comm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `demande_ami`
@@ -350,6 +372,12 @@ ALTER TABLE `images`
 --
 ALTER TABLE `langues`
   MODIFY `code_langue` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT pour la table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `notifications`
@@ -386,17 +414,18 @@ ALTER TABLE `commentaires`
   ADD CONSTRAINT `cle_secondaire` FOREIGN KEY (`id`) REFERENCES `utilisateurs` (`id`);
 
 --
--- Contraintes pour la table `etape`
---
-ALTER TABLE `etape`
-  ADD CONSTRAINT `code_comm` FOREIGN KEY (`code_comm`) REFERENCES `commentaires` (`code_comm`);
-
---
 -- Contraintes pour la table `images`
 --
 ALTER TABLE `images`
   ADD CONSTRAINT `codeEtape` FOREIGN KEY (`code_etape`) REFERENCES `etape` (`code_etape`),
   ADD CONSTRAINT `codeVoyage` FOREIGN KEY (`code_voyage`) REFERENCES `voyages` (`code_voyage`);
+
+--
+-- Contraintes pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `IdUser` FOREIGN KEY (`id`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `IdVoyage` FOREIGN KEY (`code_voyage`) REFERENCES `voyages` (`code_voyage`);
 
 --
 -- Contraintes pour la table `signalements`
